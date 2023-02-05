003c81f1f264dcec54e5a4c2e988dfc90b78e0d3
Dylarno <medeirosdylan@hotmail.com>
2023-02-04 22:48:02 -0500

    Health Bar

--- Assets/Scripts/PlayerProperties.cs
+++ Assets/Scripts/PlayerProperties.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEditor.Experimental;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerProperties : MonoBehaviour
 {
+    [SerializeField] private float maxLife = 30f;
+
+    [SerializeField] private Slider lifeBar;
+    [SerializeField] private Image lifeBarFill;
+    [SerializeField] private Gradient lifeBarGradient;
+
     public float Life
     {
         get { return life; }
         set
         {
             life = value;
+
+            lifeBar.value = value;
+            lifeBarFill.color = lifeBarGradient.Evaluate(lifeBar.normalizedValue);
         }
     }
 
